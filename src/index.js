b7fae80e92b9cc073b85fde12cea48bc7acec77c
ars-abs <ars@abstractit.in>
2023-08-15 20:26:25 +0530

    Introduced setup.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-const main = () => {
+const setup = () => {
+	const resources = {
+		users: {
+			schema: {
+				type: 'object',
+				properties: {
+					user: { type: 'string' },
+					iss: { type: 'string' },
+					refreshToken: { type: 'string' },
+					accessToken: { type: 'string' },
+				},
+			},
+			repo: 'postgres',
+		},
+	};
 
+	return {
+		resources,
+	};
 };
 
-export {
-	main,
-};
+export { setup };
