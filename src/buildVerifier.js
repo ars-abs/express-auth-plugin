81679103838cb96e48d466e36c5148320c836673
ars-abs <ars@abstractit.in>
2023-08-17 12:25:37 +0530

    Configured renewURL..

--- src/buildVerifier.js
+++ src/buildVerifier.js
@@ -3,14 +3,14 @@ import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 
 const cookieExtractor = (req) => (req && req.cookies ? req.cookies.token : '');
 
-const buildVerifier = () => {
+const buildVerifier = ({ config: { auth: { renewURL }}}) => {
 	passport.use('jwt', new JwtStrategy({
 		jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
 		secretOrKey: process.env.JWTSECRET,
 	}, (jwtPayload, done) => done(null, jwtPayload)));
 
 	return passport.authenticate('jwt',
-		{ failureRedirect: '/renewTokens', session: false });
+		{ failureRedirect: renewURL, session: false });
 };
 
 export default buildVerifier;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import setupAuthFlows from './setupAuthFlows';
 import enrichContext from './enrichContext';
 import setupRoutes from './setupRoutes';
 import buildVerifier from './buildVerifier';
+import { peek } from '@laufire/utils/debug';
 
 const setup = (context) => {
 	const enrichedContext = enrichContext(context);
 
+	peek(enrichedContext.config.auth, 'auth');
+
 	setupAuthFlows(enrichedContext);
 
 	const routes = setupRoutes(enrichedContext);
-	const validateLogin = buildVerifier();
+	const validateLogin = buildVerifier(enrichedContext);
 
 	return {
 		resources,
