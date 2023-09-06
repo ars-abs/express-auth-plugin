1f38777ece83658ae0af9e431b00ad721b85f0ab
ars-abs <ars@abstractit.in>
2023-09-06 15:52:22 +0530

    Introduced getSession.

--- src/renewAccessToken.js
+++ src/renewAccessToken.js
@@ -1,13 +1,14 @@
+/* eslint-disable max-statements */
 /* eslint-disable max-lines-per-function */
 import { findIndex } from '@laufire/utils/collection';
 import jwt from 'jsonwebtoken';
 
 const renewAccessToken = (req, res) => {
 	const { context: { config: { auth: { providers }}}} = req;
-	const token = req.cookies.refToken;
 	const secretKey = process.env.JWTSECRET;
 
 	try {
+		const token = req.cookies.refToken;
 		const { sub, iss, role } = jwt.verify(token, secretKey);
 		const issuer = findIndex(providers,
 			({ issuer: provider }) => provider === iss);
@@ -16,15 +17,20 @@ const renewAccessToken = (req, res) => {
 			process.env.JWTSECRET,
 			{ expiresIn: '15m' }
 		);
+		const statusCode = 200;
 
 		res.cookie(
-			'token', accessToken, { httpOnly: true }
-		).json({});
+			'token', accessToken, { httpOnly: true, domain: 'localhost' }
+		);
+		res.status(statusCode).json({ statusCode });
 	}
 	catch (error) {
-		error.name === 'TokenExpiredError'
-			? res.json({ error: { message: 'JWT has expired' }})
-			: res.json({ error: { message: 'JWT verification failed' }});
+		const statusCode = 401;
+
+		res.status(statusCode).json({ statusCode });
+	// 	error.name === 'TokenExpiredError'
+	// 		? res.json({ error: { message: 'JWT has expired' }})
+	// 		: res.json({ error: { message: 'JWT verification failed' }});
 	}
 };
 
--- src/setupRoutes.js
+++ src/setupRoutes.js
@@ -3,24 +3,52 @@ import authenticate from './authenticate';
 import logout from './logout';
 import buildEnrichReq from './buildEnrichReq';
 import renewAccessToken from './renewAccessToken';
+import verifyAccess from './verifyAccess';
+import jwt from 'jsonwebtoken';
 
-const redirect = (req, res) => res.redirect('/');
+const redirect = (req, res) => res.redirect('http://localhost:6006/');
 const home = (req, res) => res.send('home');
 const login = (req, res) => res.send('please choose provider');
+const getSession = (req, res) => {
+	try {
+		const { exp } = jwt.decode(req.cookies.token);
+		const statusCode = 200;
+		const secondsOffset = 1000;
+		const expiresAt = new Date(exp * secondsOffset);
+
+		res.status(statusCode).json({ statusCode, expiresAt });
+	}
+	catch (error) {
+		const statusCode = 401;
+
+		res.status(statusCode).json({ statusCode });
+	}
+};
+
+const allowCredentials = (
+	req, res, next
+) => {
+	res.header('Content-Type', 'application/json;charset=UTF-8');
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+};
 
 const setupRoutes = (context) => {
 	const { config: { auth: {
-		loginURL, logoutURL,	callbackURL, renewURL,
+		loginURL, logoutURL,	callbackURL, renewURL, session,
 	}}} = context;
 	const enrichReq = buildEnrichReq(context);
 
 	return {
-		'GET /': home,
+		'GET /': [verifyAccess, home],
 		[`GET ${ loginURL }`]: login,
-		[`GET ${ loginURL }/:provider`]: [enrichReq, authenticate],
-		[`GET ${ callbackURL }/:provider`]: [enrichReq, authenticate, saveLogin, redirect],
+		[`GET ${ loginURL }/:provider`]: [enrichReq, allowCredentials, authenticate],
+		[`GET ${ callbackURL }/:provider`]: [enrichReq, allowCredentials, authenticate, saveLogin, redirect],
 		[`GET ${ logoutURL }`]: [enrichReq, logout],
-		[`GET ${ renewURL }`]: [enrichReq, renewAccessToken],
+		[`GET ${ renewURL }`]: [enrichReq, allowCredentials, renewAccessToken],
+		[`GET ${ session }`]: [allowCredentials, getSession],
 	};
 };
 
