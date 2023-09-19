30fa453696d1028bfed8a2a12d1783188931870d
ars-abs <ars@abstractit.in>
2023-09-19 17:57:56 +0530

    Bug fixed.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import setupRoutes from './setupRoutes';
 import { self } from '@laufire/utils/fn';
 import authenticate from './verifyAccess';
 import readSession from './middlewares/readSession';
-import allowCredentials from './middlewares/allowCredentials';
-import { map } from '@laufire/utils/collection';
 
 const init = (context) => {
 	const enrichedContext = enrichContext(context);
@@ -14,8 +12,7 @@ const init = (context) => {
 	setupAuthFlows(enrichedContext);
 
 	const routes = setupRoutes(enrichedContext);
-	const middleware = (...args) =>
-		map([allowCredentials, readSession], (fn) => fn(...args));
+	const middleware = readSession;
 
 	return {
 		resources,
