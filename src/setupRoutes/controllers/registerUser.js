731abe496c901458d1a9235dfedbbc7d00252a07
ars-abs <ars@abstractit.in>
2023-10-23 16:55:02 +0530

    Set token role as user.

--- src/setupRoutes/controllers/registerUser.js
+++ src/setupRoutes/controllers/registerUser.js
@@ -1,20 +1,20 @@
-const registerUser = async (req, res) => {
-	const { body: payload } = req;
-	const {
-		session, store,
-	} = req.context;
+import setTokens from './setTokens';
 
-	const { id: identityID } = session;
+const registerUser = async (req, res) => {
+	const { session: { id: identityID }, store	} = req.context;
+
 	const { data: { id: userID }} = await store({
 		...req.context, name: 'users', action: 'create',
-		data: { payload },
+		data: { payload: req.body },
 	});
 
 	await store({
 		...req.context, name: 'userIdentityMap', action: 'create',
 		data: { payload: { userID, identityID }},
 	});
+	const payload = { id: userID, role: 'user' };
 
+	setTokens({ req, res, payload });
 	res.json({ data: { message: 'success' }});
 };
 
