7f8de4ebdce76cef6e50ca511fa8569c75858072
ars-abs <ars@abstractit.in>
2023-10-24 15:34:52 +0530

    Refactored authenticate.

--- src/includeAuthenticate.js
+++ src/includeAuthenticate.js
@@ -1,12 +1,22 @@
+import { keys } from '@laufire/utils/collection';
+
+const controller = {
+	allowed: (roles, role) => roles.allowed.includes(role),
+	denied: (roles, role) => !roles.denied.includes(role),
+};
 
 const includeAuthenticate = () => ({
-	authenticate: ({ session, next }) =>
-		(session.role === 'guest'
-			? {
+	authenticate: ({ session: { role }, config, name, next }) => {
+		const { auth: { roles }} = config.resources[name];
+		const target = keys(roles)[0];
+
+		return controller[target](roles, role)
+			? next()
+			: {
 				meta: { status: 'unauthorized' },
 				error: { message: 'Unauthorized access.' },
-			}
-			: next()),
+			};
+	},
 });
 
 export default includeAuthenticate;
