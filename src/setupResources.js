6335fff90d84ff46ac43b0c92736051011a6f5e7
ars-abs <ars@abstractit.in>
2023-10-25 17:21:26 +0530

    Setup configurable auth resources.

--- src/setupResources.js
+++ src/setupResources.js
@@ -1,59 +1,64 @@
-const resources = {
-	users: {
-		schema: {
-			properties: {
-				name: { type: 'string' },
-			},
-		},
-		repo: 'postgres',
-	},
-	roles: {
-		schema: {
-			properties: {
-				name: { type: 'string' },
-			},
-		},
-		repo: 'postgres',
-	},
-	identity: {
-		schema: {
-			properties: {
-				provider: { type: 'string' },
-				providerID: { type: 'string' },
-			},
-		},
-		repo: 'postgres',
-	},
-	userIdentityMap: {
-		includes: ['users', 'identity'],
-		schema: {
-			properties: {
-				userID: {
-					type: 'string', format: 'ref', entity: 'users',
-				},
-				identityID: {
-					type: 'string', format: 'ref', entity: 'identity',
-				},
-			},
-		},
-		repo: 'postgres',
-	},
-	userRoleMap: {
-		includes: ['users', 'roles'],
-		schema: {
-			properties: {
-				userID: {
-					type: 'string', format: 'ref', entity: 'users',
-				},
-				roleID: {
-					type: 'string', format: 'ref', entity: 'roles',
-				},
-			},
-		},
-		repo: 'postgres',
-	},
-};
+/* eslint-disable max-lines-per-function */
 
-const setupResources = () => ({ resources });
+const setupResources = ({ config: { auth: {
+	resources: { users, roles, identity, userIdentityMap, userRoleMap },
+	repo,
+}}}) => ({
+	resources: {
+		[users]: {
+			schema: {
+				properties: {
+					name: { type: 'string' },
+				},
+			},
+			repo: repo,
+		},
+		[roles]: {
+			schema: {
+				properties: {
+					name: { type: 'string' },
+				},
+			},
+			repo: repo,
+		},
+		[identity]: {
+			schema: {
+				properties: {
+					provider: { type: 'string' },
+					providerID: { type: 'string' },
+				},
+			},
+			repo: repo,
+		},
+		[userIdentityMap]: {
+			includes: [users, identity],
+			schema: {
+				properties: {
+					userID: {
+						type: 'string', format: 'ref', entity: users,
+					},
+					identityID: {
+						type: 'string', format: 'ref', entity: identity,
+					},
+				},
+			},
+			repo: repo,
+		},
+		[userRoleMap]: {
+			includes: [users, roles],
+			schema: {
+				properties: {
+					userID: {
+						type: 'string', format: 'ref', entity: users,
+					},
+					roleID: {
+						type: 'string', format: 'ref', entity: roles,
+					},
+				},
+			},
+			repo: repo,
+		},
+	},
+});
 
 export default setupResources;
