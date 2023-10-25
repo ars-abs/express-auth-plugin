ac859834c0b10be14d461f81b0c87e0430108845
ars-abs <ars@abstractit.in>
2023-10-25 15:10:17 +0530

    Introduce some resources.

--- src/setupResources.js
+++ src/setupResources.js
@@ -7,6 +7,14 @@ const resources = {
 		},
 		repo: 'postgres',
 	},
+	roles: {
+		schema: {
+			properties: {
+				name: { type: 'string' },
+			},
+		},
+		repo: 'postgres',
+	},
 	identity: {
 		schema: {
 			properties: {
@@ -25,6 +33,20 @@ const resources = {
 				},
 				identityID: {
 					type: 'string', format: 'ref', entity: 'identity',
+				},
+			},
+		},
+		repo: 'postgres',
+	},
+	userRoleMap: {
+		includes: ['users', 'roles'],
+		schema: {
+			properties: {
+				userID: {
+					type: 'string', format: 'ref', entity: 'users',
+				},
+				roleID: {
+					type: 'string', format: 'ref', entity: 'roles',
 				},
 			},
 		},
